2ba9d4ed13597c0ec26c68f5eea44df3e605151f
ahmed55719 <mrmrx514@gmail.com>
2025-11-08 20:34:10 +0200

    Implement localStorage for list item management
    Add functionality to save and load list items from localStorage.

--- aa.js
+++ aa.js
@@ -1,14 +1,31 @@
-           let ul=document.querySelector("ul");
-           let count=1;
+let ul = document.querySelector("ul");
+let count = 1;
+window.onload = function() {
+    const savedItems = JSON.parse(localStorage.getItem("laaList")) || [];
+    savedItems.forEach(item => {
+        const li = document.createElement("li");
+        li.innerHTML = item;
+        ul.appendChild(li);
+    });
+    count = savedItems.length + 1;
+};
+function saveList() {
+    const items = Array.from(ul.children).map(li => li.innerHTML);
+    localStorage.setItem("laaList", JSON.stringify(items));
+}
 function addListItem() {
-    let ul=document.querySelector("ul");
-    let li =document.createElement("li");
-    li.innerHTML="لأ "+count;
+    const li = document.createElement("li");
+    li.innerHTML = "لأ " + count;
     ul.appendChild(li);
     count++;
-};
+    saveList();
+}
+
 
 function remove() {
-ul.lastElementChild.remove();
-    count--;
-};
+    if (ul.lastElementChild) {
+        ul.lastElementChild.remove();
+        count--;
+        saveList(); 
+    }
+}
